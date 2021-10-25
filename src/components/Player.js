08dd75808155f99df666b7ea94278701732e9a03
Smiti <mishra.smiti06@gmail.com>
2021-10-25 13:32:23 -0400

    understanding the data

--- src/components/Player.js
+++ src/components/Player.js
@@ -4,7 +4,7 @@ function Player(){
       <h1>John <span>Secret</span> Doe</h1>
       <h2 className="zero">Currently with no wins :(</h2>
       <h2>Currently at 1 win</h2>
-      <h2>Currently at 1+ wins</h2>)
+      <h2>Currently at 1+ wins</h2>
     </article>
   );
 }
--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,6 +1,7 @@
 import playerData from '../data/playerData';
 import matchData from '../data/matchData';
 import Player from "./Player";
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
 function PlayerList (props) {
   return (
